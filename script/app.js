347b57c3f1a7a662c3a86a6cadd54e52f98a5f45
RL Tuprio <78067372+leemonnadee@users.noreply.github.com>
2022-03-15 12:58:34 +0800

    iCargo
    Sample

---
+++ script/app.js
@@ -0,0 +1,14 @@
+function toSignUp(){
+var hideSignIn=document.getElementById("login");
+var showSignup=document.getElementById("signup");
+hideSignIn.style.display="none";
+showSignup.style.display="block";
+
+}
+function toSignIn(){
+    var hideSignUp=document.getElementById("signup");
+    var ShowLogin=document.getElementById("login");
+    hideSignUp.style.display="none";
+    ShowLogin.style.display="block";
+
+}
